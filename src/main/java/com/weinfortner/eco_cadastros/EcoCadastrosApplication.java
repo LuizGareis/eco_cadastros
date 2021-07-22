e639683c5bafde638315099c442bd7dd50822b80
LuizGareis <luiz_gareis@hotmail.com>
2021-07-21 21:09:48 -0300

    Primeiro commit

---
+++ src/main/java/com/weinfortner/eco_cadastros/EcoCadastrosApplication.java
@@ -0,0 +1,13 @@
+package com.weinfortner.eco_cadastros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoCadastrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoCadastrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weinfortner/eco_cadastros/EcoCadastrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weinfortner.eco_cadastros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoCadastrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
